9753900a61d15ed106bdf9797e3599d0da8c166c
helloheesu <heesu.jung.developer@gmail.com>
2022-01-07 11:13:41 +0900

    responsive container
    this closes #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import './App.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Container = styled.div`
+  margin: 2em;
+  background-color: #eee;
+  min-width: 300px;
+  min-height: 450px;
+  max-width: 900px;
+  max-height: 1350px;
+  height: 80%;
+  width: 80%;
+`;
 
 function App() {
-  return <div className="App">HI</div>;
+  return (
+    <Wrapper>
+      <Container></Container>
+    </Wrapper>
+  );
 }
 
 export default App;
