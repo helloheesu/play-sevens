cfae5cb86047f64a4f6302dac8994c580a1b8620
helloheesu <heesu.jung.developer@gmail.com>
2022-01-08 17:47:07 +0900

    style cards - color and font

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,9 +1,41 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const COLOR_PALETTE = [
+  // https://colorhunt.co/palette/d9d7f1fffddee7fbbeffcbcb
+  '#FFCBCB',
+  '#E7FBBE',
+  '#FFFDDE',
+  '#D9D7F1',
+];
+
+const getColorIndex = (value: number) => {
+  switch (value) {
+    case 1:
+      return 0;
+    case 2:
+      return 1;
+    default:
+      return 2;
+  }
+};
+
+interface ContainerProps {
+  colorIndex: number;
+  row: number;
+  col: number;
+}
+const Container = styled.div<ContainerProps>`
   background-color: pink;
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  background-color: ${(props) => COLOR_PALETTE[props.colorIndex]};
+  grid-row: ${(props) => `${props.row + 1}/${props.row + 2}`};
+  grid-column: ${(props) => `${props.col + 1}/${props.col + 2}`};
 `;
 
 interface Props {
@@ -13,12 +45,7 @@ interface Props {
 }
 const Card = ({ row, col, value }: Props) => {
   return (
-    <Container
-      style={{
-        gridRow: `${row + 1}/${row + 2}`,
-        gridColumn: `${col + 1}/${col + 2}`,
-      }}
-    >
+    <Container colorIndex={getColorIndex(value)} row={row} col={col} style={{}}>
       {value}
     </Container>
   );
