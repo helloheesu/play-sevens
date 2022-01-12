35247498b614f80517774d50134fb0ee9534089d
helloheesu <heesu.jung.developer@gmail.com>
2022-01-12 18:00:31 +0900

    refactor - move logics into reducer

--- src/gridToLine.ts
+++ src/gridToLine.ts
@@ -1,37 +1,27 @@
-const gridToLine = (ROW_SIZE: number, COL_SIZE: number) => {
-  const generateRandomRowIndex = () => {
-    return Math.floor(Math.random() * ROW_SIZE);
-  };
-  const generateRandomColIndex = () => {
-    return Math.floor(Math.random() * COL_SIZE);
-  };
-
-  const getIndex = (row: number, col: number) => row * COL_SIZE + col;
-  const getRightIndex = (row: number, col: number) =>
-    col > COL_SIZE - 2 ? null : row * COL_SIZE + col + 1;
-  const getLeftIndex = (row: number, col: number) =>
-    col < 1 ? null : row * COL_SIZE + col - 1;
-  const getDownIndex = (row: number, col: number) =>
-    row > ROW_SIZE - 2 ? null : (row + 1) * COL_SIZE + col;
-  const getUpIndex = (row: number, col: number) =>
-    row < 1 ? null : (row - 1) * COL_SIZE + col;
-
-  const getGridIndexFromLineIndex = (index: number) => ({
-    row: Math.floor(index / COL_SIZE),
-    col: index % COL_SIZE,
-  });
-
-  return {
-    generateRandomRowIndex,
-    generateRandomColIndex,
-    getIndex,
-    getRightIndex,
-    getLeftIndex,
-    getDownIndex,
-    getUpIndex,
-    getGridIndexFromLineIndex,
-    LINE_SIZE: ROW_SIZE * COL_SIZE,
-  };
+export const generateRandomRowIndex = (rowSize: number) => {
+  return Math.floor(Math.random() * rowSize);
+};
+export const generateRandomColIndex = (colSize: number) => {
+  return Math.floor(Math.random() * colSize);
 };
 
-export default gridToLine;
+interface Payload {
+  row: number;
+  col: number;
+  rowSize: number;
+  colSize: number;
+}
+export const getIndex = ({ row, col, colSize }: Payload) => row * colSize + col;
+export const getRightIndex = ({ row, col, colSize }: Payload) =>
+  col > colSize - 2 ? null : row * colSize + col + 1;
+export const getLeftIndex = ({ row, col, colSize }: Payload) =>
+  col < 1 ? null : row * colSize + col - 1;
+export const getDownIndex = ({ row, col, rowSize, colSize }: Payload) =>
+  row > rowSize - 2 ? null : (row + 1) * colSize + col;
+export const getUpIndex = ({ row, col, colSize }: Payload) =>
+  row < 1 ? null : (row - 1) * colSize + col;
+
+export const getGridIndexFromLineIndex = (index: number, colSize: number) => ({
+  row: Math.floor(index / colSize),
+  col: index % colSize,
+});
