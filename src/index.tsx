6ec8cc886720e92a747e0195516cf2f476cd53bc
helloheesu <heesu.jung.developer@gmail.com>
2022-01-12 20:16:54 +0900

    uninstall useless recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import { logEvent } from 'firebase/analytics';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RecoilRoot } from 'recoil';
 import App from './App';
 import { analytics } from './fbase';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
