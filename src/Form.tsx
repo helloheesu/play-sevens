8673c801d743d21a9acd29edec0edfc25c7654d1
helloheesu <heesu.jung.developer@gmail.com>
2022-01-14 21:47:26 +0900

    markup submit form

---
+++ src/Form.tsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+const BORDER_RADIUS = '0.3rem';
+const Container = styled.form`
+  margin-top: 1em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > * {
+    box-sizing: border-box;
+    text-align: center;
+  }
+
+  input {
+    width: 10em;
+  }
+
+  input[type='text'] {
+    font-size: 0.8em;
+    margin: 0.5em ${BORDER_RADIUS};
+    padding: 0.2em 0;
+    border: none;
+    border-bottom: 2px solid ${(props) => props.theme.background.main};
+    outline: none;
+    &:focus {
+      border-bottom-color: ${(props) => props.theme.blue.main};
+    }
+  }
+
+  input[type='submit'] {
+    margin: 0 auto;
+    padding: 0.3em;
+    font-size: 0.8em;
+    font-weight: 700;
+    background: ${(props) => props.theme.blue.main};
+    color: ${(props) => props.theme.white.main};
+    border: none;
+    border-radius: ${BORDER_RADIUS};
+  }
+
+  input[type='button'] {
+    margin-top: 1em;
+    background: none;
+    border: none;
+    outline: none;
+    color: ${(props) => props.theme.background.darken};
+  }
+`;
+const INPUT_ID = 'score-submit-name';
+const Form = () => {
+  return (
+    <Container>
+      <label htmlFor={INPUT_ID}>Your Name:</label>
+      {/* reason for 'size': https://www.geeksforgeeks.org/how-to-specify-the-width-of-an-input-element-in-html5/ */}
+      <input
+        id={INPUT_ID}
+        type="text"
+        size={1}
+        spellCheck="false"
+        autoFocus
+        required
+        // autoComplete="off" // [NOTE] automcomplete covers error message
+      />
+      <input type="submit" value="Submit" />
+      <input type="button" value="close" />
+    </Container>
+  );
+};
+
+export default Form;
