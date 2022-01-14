053b27461809daf1b2a8f4d618630b31b2f6d29e
helloheesu <heesu.jung.developer@gmail.com>
2022-01-14 18:54:41 +0900

    responsive grid size - not logic implemented yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import {
+  useEffect,
+  useLayoutEffect,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import Card from './Card';
 import Modal from './Modal';
@@ -6,6 +12,18 @@ import reducer from './reducer';
 import { getGridIndexFromLineIndex } from './gridToLine';
 import defaultTheme from './theme';
 import ArrowButtonsLayer from './ArrowButtonsLayer';
+import { CELL_GAP_PX, CELL_HEIGHT_PX, CELL_WIDTH_PX } from './consts';
+
+const calculateGridSize = (containerWidth: number, containerHeight: number) => {
+  return {
+    col: Math.floor(
+      (containerWidth - CELL_GAP_PX) / (CELL_WIDTH_PX + CELL_GAP_PX)
+    ),
+    row: Math.floor(
+      (containerHeight - CELL_GAP_PX) / (CELL_HEIGHT_PX + CELL_GAP_PX)
+    ),
+  };
+};
 
 const Wrapper = styled.div`
   width: 100%;
@@ -14,33 +32,63 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  padding: 2rem;
+  box-sizing: border-box;
 `;
 const Container = styled.div`
-  margin: 2em;
   background-color: ${(props) => props.theme.background.main};
   position: relative;
+  width: 100%;
+  flex-grow: 1;
 `;
 
 const Grid = styled.div<{ row: number; col: number }>`
   display: grid;
-  padding: 1em;
-  gap: 1em;
+  padding: ${CELL_GAP_PX}px;
+  gap: ${CELL_GAP_PX}px;
   box-sizing: border-box;
   grid-template: ${(props) =>
-    `repeat(${props.row}, 1fr) / repeat(${props.col}, 1fr)`};
+    `repeat(${props.row}, minmax(${CELL_HEIGHT_PX}px, auto)) / repeat(${props.col}, minmax(${CELL_WIDTH_PX}px, auto))`};
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-items: center;
 `;
 const Cell = styled.div`
   background-color: ${(props) => props.theme.background.darken};
-  width: ${(props) => props.theme.cellWidth};
-  height: ${(props) => props.theme.cellHeight};
+  width: ${CELL_WIDTH_PX}px;
+  height: ${CELL_HEIGHT_PX}px;
 `;
 
 function App() {
-  const ROW_SIZE = 5;
-  const COL_SIZE = 3;
+  const gridContainerRef = useRef<HTMLDivElement>(null);
+  const [gridCol, setGridCol] = useState(0);
+  const [gridRow, setGridRow] = useState(0);
+
+  const onResize = () => {
+    if (gridContainerRef && gridContainerRef.current) {
+      const { row, col } = calculateGridSize(
+        gridContainerRef.current.clientWidth,
+        gridContainerRef.current.clientHeight
+      );
+      setGridCol(col);
+      setGridRow(row);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+  useLayoutEffect(() => {
+    onResize();
+  }, []);
+  useEffect(() => {
+    console.log(gridRow, gridCol);
+  }, [gridRow, gridCol]);
+
   const [state, dispatch] = useReducer(reducer, {
-    rowSize: ROW_SIZE,
-    colSize: COL_SIZE,
+    rowSize: 0,
+    colSize: 0,
     cardSlots: [],
     initialCardCount: 1,
     newCardValues: [1, 1, 1, 2, 2, 2, 3],
@@ -114,19 +162,15 @@ function App() {
             <Card value={state.nextNewCardValue} />
           </Cell>
           {state.isGameEnded && <Modal score={calculateTotalScore()} />}
-          <Container>
-            <Grid
-              row={ROW_SIZE}
-              col={COL_SIZE}
-              style={{ position: 'absolute' }}
-            >
-              {Array.apply(null, Array(ROW_SIZE * COL_SIZE)).map((_, i) => (
+          <Container ref={gridContainerRef}>
+            <Grid row={gridRow} col={gridCol} style={{ position: 'absolute' }}>
+              {Array.apply(null, Array(gridRow * gridCol)).map((_, i) => (
                 <Cell key={i} />
               ))}
             </Grid>
-            <Grid row={ROW_SIZE} col={COL_SIZE}>
+            <Grid row={gridRow} col={gridCol}>
               {state.cardSlots.map((card, index) => {
-                const { row, col } = getGridIndexFromLineIndex(index, COL_SIZE);
+                const { row, col } = getGridIndexFromLineIndex(index, gridCol);
                 return (
                   card && (
                     <Cell
---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const CELL_GAP_PX = 16;
+export const CELL_WIDTH_PX = CELL_GAP_PX * 3;
+export const CELL_HEIGHT_PX = CELL_GAP_PX * 4;
