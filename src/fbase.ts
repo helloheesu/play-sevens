a237bf6ffabc7bef1d243ce2bc0e644150e5becd
helloheesu <heesu.jung.developer@gmail.com>
2022-01-25 08:26:58 +0900

    GA - log event

--- src/fbase.ts
+++ src/fbase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 import {
   arrayUnion,
   collection,
@@ -30,7 +30,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
+export const logAnalytics = (eventName: string, ...args: any[]) =>
+  logEvent(analytics, eventName, ...args);
 
 const db = getFirestore(app);
 const sizesRef = collection(db, 'sizes');
