3c522843f257644687888235c24700088a598308
helloheesu <heesu.jung.developer@gmail.com>
2022-02-04 01:21:00 +0900

    set user property for package version

--- src/fbase.ts
+++ src/fbase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics, logEvent } from 'firebase/analytics';
+import { getAnalytics, logEvent, setUserProperties } from 'firebase/analytics';
 import {
   arrayUnion,
   collection,
@@ -31,6 +31,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+setUserProperties(analytics, {
+  package_version: process.env.REACT_APP_VERSION,
+});
 export const logAnalytics = (eventName: string, ...args: any[]) =>
   logEvent(analytics, eventName, ...args);
 
