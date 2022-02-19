687acf5f3a2dbdce374579577e664fcde66eeb56
helloheesu <heesu.jung.developer@gmail.com>
2022-02-20 05:18:47 +0900

    remove useless css lines

--- src/components/ResponsiveCellGrid.tsx
+++ src/components/ResponsiveCellGrid.tsx
@@ -1,27 +1,24 @@
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { Direction, getGridIndexFromLineIndex } from '../utils/gridToLine';
+import { getGridIndexFromLineIndex } from '../utils/gridToLine';
 import { Action, State } from '../reducer';
 import useResponsiveCell from '../hooks/useResponsiveCell';
 
 const Grid = styled.div<{
   row: number;
   col: number;
-  gap: number;
   width: number;
   height: number;
 }>`
   background-color: ${(props) => props.theme.background.main};
   display: grid;
-  padding: ${(props) => props.gap}px;
-  gap: ${(props) => props.gap}px;
-  box-sizing: border-box;
   grid-template: ${(props) =>
     `repeat(${props.row}, minmax(${props.height}px, auto)) / repeat(${props.col}, minmax(${props.width}px, auto))`};
   width: 100%;
   height: 100%;
   align-items: center;
   justify-items: center;
+  align-content: space-around;
   justify-content: space-around;
 `;
 const EmptyCell = styled.div<{ width: number; height: number }>`
@@ -49,12 +46,10 @@ const ResponsiveCellGrid = ({ state, dispatch, children }: Props) => {
 
   return (
     <Grid
-      ref={gridContainerRef}
       row={state.rowSize}
       col={state.colSize}
       width={cellWidth}
       height={cellHeight}
-      gap={cellGap}
     >
       {Array.apply(null, Array(state.rowSize * state.colSize)).map((_, i) => {
         const { row, col } = getGridIndexFromLineIndex(i, state.colSize);
@@ -62,7 +57,7 @@ const ResponsiveCellGrid = ({ state, dispatch, children }: Props) => {
           <EmptyCell
             key={i}
             width={cellWidth}
-            height={cellHeight - 5}
+            height={cellHeight}
             style={{
               gridRow: `${row + 1}/${row + 2}`,
               gridColumn: `${col + 1}/${col + 2}`,
